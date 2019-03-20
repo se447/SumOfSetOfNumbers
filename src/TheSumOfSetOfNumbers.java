28d0d5ad27ccd14fa3c9733c84dcfdc6bebd4760
Saad E <21321555+se447@users.noreply.github.com>
2019-03-19 20:31:40 -0400

    Created while loop to calculate sum of set of numbers

--- src/TheSumOfSetOfNumbers.java
+++ src/TheSumOfSetOfNumbers.java
@@ -8,6 +8,20 @@ public class TheSumOfSetOfNumbers {
 
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
-
+        
+        //read int n from user
+        System.out.println("Until what? ");
+        int limit = Integer.parseInt(reader.nextLine());
+        
+        // need sum, iterator count in loop
+        int sum = 0;
+        int i = 1; //iterator
+        
+        while (i <= limit) {
+            sum += i;
+            i++;
+        }
+        
+        System.out.println("Sum is " + sum);
     }
 }
