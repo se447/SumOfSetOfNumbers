b875d84b56e04bcb48e4ec0b38d8344bcbb311c7
Saad E <21321555+se447@users.noreply.github.com>
2019-03-19 20:25:01 -0400

    defined java class and import statement

---
+++ src/TheSumOfSetOfNumbers.java
@@ -0,0 +1,13 @@
+/*
+Sum of a set of numbers
+Create a program that calculates the sum 1+2+3+…+n where n is a number entered by the user.
+*/
+import java.util.Scanner;
+
+public class TheSumOfSetOfNumbers {
+
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+
+    }
+}
